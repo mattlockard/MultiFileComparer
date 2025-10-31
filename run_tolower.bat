41057591dda329737daea377dad5416afec79637
Matt Lockard <mslockard@gmail.com>
2025-10-31 19:26:54 -0400

    adding another bat file that was missed

---
+++ run_tolower.bat
@@ -0,0 +1,13 @@
+@echo off
+REM Create virtual environment if it doesn't exist
+if not exist venv (
+    python -m venv venv
+)
+
+REM Activate the virtual environment
+call venv\Scripts\activate
+
+REM Run the FileComparer script with any arguments
+run_FileComparer.bat --mode lower
+
+pause
